b7599be9358625fdbe6355ef53e3b1b4dfa4bb41
Khodatskaa <khodatskaalina@gmail.com>
2023-12-10 01:00:14 +0200

    Ex was done

--- Housing_Managment_cpp/Main.cpp
+++ Housing_Managment_cpp/Main.cpp
@@ -16,15 +16,17 @@ int main()
         cout << "\n       Housing Management System\n";
         cout << "-------------------------------------------\n";
         cout << "\n1. Add Resident\n";
-        cout << "2. Remove Resident\n";
-        cout << "3. Add Apartment\n";
-        cout << "4. Remove Apartment\n";
-        cout << "5. Attach Resident To Apartment\n";
-        cout << "6. Detach Resident From Apartment\n";
-        cout << "7. Save Information\n";
-        cout << "8. Load Information\n";
-        cout << "9. Create Report\n";
-        cout << "10. Quit\n";
+        cout << "2. Edit Resident\n";
+        cout << "3. Remove Resident\n";
+        cout << "4. Add Apartment\n";
+        cout << "5. Edit Apartment\n";
+        cout << "6. Remove Apartment\n";
+        cout << "7. Attach Resident To Apartment\n";
+        cout << "8. Detach Resident From Apartment\n";
+        cout << "9. Save Information\n";
+        cout << "10. Load Information\n";
+        cout << "11. Create Report\n";
+        cout << "12. Quit\n";
         cout << "\nEnter your choice: ";
         cin >> choice;
 
@@ -33,30 +35,36 @@ int main()
             residentManager.addResident();
             break;
         case 2:
+            residentManager.editResident();
+			break;
+        case 3:
             residentManager.removeResident();
             break;
-        case 3:
+        case 4:
             apartmentManager.addApartment();
             break;
-        case 4:
+        case 5:
+            apartmentManager.editApartment();
+			break;
+        case 6:
             apartmentManager.removeApartment();
             break;
-        case 5:
+        case 7:
             apartmentManager.attachResidentToApartment();
             break;
-        case 6:
+        case 8:
             apartmentManager.detachResidentFromApartment();
             break;
-        case 7:
+        case 9:
             residentManager.saveInformation();
             break;
-        case 8:
+        case 10:
             residentManager.loadInformation();
             break;
-        case 9:
+        case 11:
             residentManager.report();
             break;
-        case 10:
+        case 12:
             cout << "Exiting program\n";
             break;
         default:
