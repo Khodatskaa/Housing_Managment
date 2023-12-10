d637d4086b1d43a85f64f28781df6280daf10d22
Khodatskaa <khodatskaalina@gmail.com>
2023-12-10 12:26:55 +0200

    Ex was done

--- Housing_Managment_cpp/Data.h
+++ Housing_Managment_cpp/Data.h
@@ -3,7 +3,7 @@
 
 #include <string>
 
-class Data {
+struct Data {
 public:
     static void writeToFile(const std::string& filename, const std::string& data);
     static std::string readFromFile(const std::string& filename);
